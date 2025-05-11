899b82ee2753f1109951119e85eba8ee248a2e8e
10PercentG <yousuf.alawadhi99@gmail.com>
2025-05-11 12:58:32 +0400

    test

--- api/classify.js
+++ api/classify.js
@@ -2,19 +2,22 @@
 const fs   = require('fs');
 const path = require('path');
 
-// dynamic import so we can use node-fetch v3+ in CJS
+// dynamic ESM import of node-fetch
 const fetch = (...args) =>
   import('node-fetch').then(m => m.default(...args));
 
-// load your Custom Vision project ID & iteration name
+// load Custom Vision config
 const { projectId, publishName } = JSON.parse(
   fs.readFileSync(path.join(__dirname, '../cv-config.json'))
 );
 
-// build the Custom Vision REST endpoint for raw-image calls
 const PRED_ENDPOINT =
   process.env.PREDICTION_ENDPOINT.replace(/\/$/, '') +
-  `/customvision/v3.0/Prediction/${projectId}/classify/iterations/${publishName}/image`;
+  `/customvision/v3.0/Prediction/` +
+  projectId +
+  `/classify/iterations/` +
+  publishName +
+  `/image`;
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
@@ -23,8 +26,7 @@ module.exports = async (req, res) => {
   }
   try {
     const { imageBase64 } = req.body;
-    // strip off "data:image/jpeg;base64,"
-    const b64 = imageBase64.split(',')[1];
+    const b64    = imageBase64.split(',')[1];
     const buffer = Buffer.from(b64, 'base64');
 
     const apiRes = await fetch(PRED_ENDPOINT, {
@@ -40,12 +42,9 @@ module.exports = async (req, res) => {
       const txt = await apiRes.text();
       return res.status(apiRes.status).send(txt);
     }
-
     const json = await apiRes.json();
-    const best = json.predictions.reduce((a,b) =>
-      a.probability > b.probability ? a : b
-    );
-    return res.status(200).json({ tag: best.tagName, probability: best.probability });
+    const best = json.predictions.reduce((a,b)=> a.probability>b.probability?a:b );
+    return res.json({ tag: best.tagName, probability: best.probability });
   } catch (err) {
     console.error(err);
     return res.status(500).send('Internal Server Error');
